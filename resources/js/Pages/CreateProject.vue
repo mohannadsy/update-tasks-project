b990aa41eb2b68e7dab9e05ee9caf1d9213c5c29
SaraAbdo96 <sara.nadim.abdo@gmail.com>
2022-05-09 21:21:56 -0700

    first test

---
+++ resources/js/Pages/CreateProject.vue
@@ -0,0 +1,32 @@
+<template>
+    <Head title="Dashboard" />
+
+    <BreezeAuthenticatedLayout>
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                Create
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">اسم المشروع</label>
+                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="اسم المشروع...">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlTextarea1" class="form-label">الوصف</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </BreezeAuthenticatedLayout>
+</template>
+
+<script>
+
+</script>
